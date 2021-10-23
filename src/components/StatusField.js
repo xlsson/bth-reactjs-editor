89ea210e25b14a0bcb1b75a1a76996d261c16a9e
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-10-23 14:27:18 +0200

    New component for status field

--- src/components/StatusField.js
+++ src/components/StatusField.js
@@ -3,10 +3,20 @@ import React from 'react';
 class StatusField extends React.Component {
     render() {
         return (
-            <div className="flex-column">
+            <div className="flex-row">
                 <>
-                <div className="field-title">Logged in as: {this.props.currentUserEmail}</div>
-                <div className="field-title">Document owner: {this.props.currentOwnerEmail}</div>
+                    <div className="flex-column justify-flex-end">
+                        <>
+                        <div className="status-label">Logged in as:</div>
+                        <div className="status-label">Document owner:</div>
+                        </>
+                    </div>
+                    <div className="flex-column">
+                        <>
+                        <div className="status-info">{this.props.currentUserEmail}</div>
+                        <div className="status-info">{this.props.currentOwnerEmail}</div>
+                        </>
+                    </div>
                 </>
             </div>
         );
